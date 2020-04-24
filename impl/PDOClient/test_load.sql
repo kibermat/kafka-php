145eba4d50334ee9a29ba0759c0b67c43a9f45b7
Алмаз Мустакимов <a.mustakimov@bars-open.ru>
2020-04-24 19:23:28 +0300

    bf resources_person migration

--- impl/PDOClient/test_load.sql
+++ impl/PDOClient/test_load.sql
@@ -4,8 +4,8 @@ select public.kafka_load_lpu('get-lpu-info');
 
 select public.kafka_load_profile('get-profile-info');
 
-select public.kafka_load_derections('get-directions-info');
+select public.kafka_load_derections('get-direction-info');
 
-select public.kafka_load_resource_person('get-resources-info');
+select public.kafka_load_resources('get-resource');
 
 select public.kafka_load_resource_person('get-resource-person');
